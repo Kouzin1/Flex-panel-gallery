b031ba99e8bbf133c4052809628c296c9e4ac662
Kouzin1 <samuelakinwunmi2@gmail.com>
2025-04-04 15:59:34 +0100

    Update

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
 const panels = document.querySelectorAll(".panel");
-console.log("test");
+// console.log("test");
 
 function toggleOpen() {
-  console.log("hola");
+  //   console.log("hola");
   this.classList.toggle("open");
 }
 
+function toggleActive(e) {
+  console.log(e.propertyName);
+  if (e.propertyName.includes("flex")) {
+    this.classList.toggle("open-active");
+  }
+}
+
 panels.forEach((panel) => panel.addEventListener("click", toggleOpen));
+panels.forEach((panel) =>
+  panel.addEventListener("transitionend", toggleActive)
+);
